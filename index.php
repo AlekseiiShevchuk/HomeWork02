3ce0a1344102b8b3827c7d9b4a15197801a2831e
alekseiishevchuk <shevchuka@gmail.com>
2016-05-26 18:48:45 +0300

    first version is ready

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use lift\lift;
+
+$lift = new lift(2,4);
+
+
+/*
+echo 'Enter first the number ';
+$choice = intval(fgets(STDIN));
+
+echo 'Enter second the number ';
+$choice2 = intval(fgets(STDIN));
+
+if ($choice>10){
+    echo 'Your number more then 10';
+}else {
+    echo 'Your number less then 10';
+}
+*/
